6619c228bdabe576b361d1d4b0d4842247fddad8
tiagomqbarata <tiagobarata98@gmail.com>
2022-01-16 02:07:03 +0000

    cluster

--- main.c
+++ main.c
@@ -168,7 +168,7 @@ void print_usage (char *msg) {
 
 int alloc_matrix (int *m, int N) {
 
-*m = (int*) malloc (N*sizeof(int));
+m = (int*) malloc (N*sizeof(int));
 	if (!(m)) {
 		print_usage ((char *)"Could not allocate memory for array!");
 		return 0;
