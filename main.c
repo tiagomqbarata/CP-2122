c7c1db8bfc0727b0c92f487bca4d46da0e8e8c68
tiagomqbarata <tiagobarata98@gmail.com>
2022-01-16 02:00:09 +0000

    cluster

--- main.c
+++ main.c
@@ -185,9 +185,9 @@ int init_array (int *m, int N, int max) {
 	int *ptr;
 
 	if (!alloc_matrix (m, N)) return 0;
-	for (i=0 , ptr = m ; i<N ; i++ , ptr++) {
-		*ptr = my_rand(max);
-	}
+//	for (i=0 , ptr = m ; i<N ; i++ , ptr++) {
+		//*ptr = my_rand(max);
+//	}
 	return 1;
 } 
 
